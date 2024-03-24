ab7c3d04a5362958eb9da61a2b380f9a0e3ecf93
hatilima <jasperhatilima@gmail.com>
2024-03-24 22:30:53 +0100

    (c) - adding a FreeRTOS task

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
 #include "stm32f103x_registers.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
 void delay() {
     for (int i = 0; i < 1000000; ++i);
+}
+
+static void task1(void* args)
+{
+    int i;
+    // Toggle Pin 13 (LED)
+    GPIOC->ODR ^= (1 << 13);
+    delay();
+    /*for(i = 0; i < 1000000; i++)
+    {
+        __asm__("nop");
+    } */
 }
 
 int main() {
@@ -12,11 +26,9 @@ int main() {
     GPIOC->CRH |= (1 << 20);  // Set mode bits for Pin 13
     GPIOC->CRH &= ~(1 << 21);
 
-    while (1) {
-        // Toggle Pin 13 (LED)
-        GPIOC->ODR ^= (1 << 13);
-        delay();
-    }
+    xTaskCreate(task1,"LED",100,NULL,configMAX_PRIORITIES-1,NULL);
+	vTaskStartScheduler();
+	for (;;);
 
     return 0;
 }
